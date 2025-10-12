0a77713e6efc449f8f95c94240d463636529aa02
Satont <satontworldwide@gmail.com>
2025-10-12 04:11:21 +0300

    chore: skip redis bench

--- stores/stores_bench_test.go
+++ stores/stores_bench_test.go
@@ -1,9 +1,16 @@
 package stores
 
-import "testing"
+import (
+	"testing"
+)
 
 func BenchmarkGet(b *testing.B) {
 	for _, impl := range implementations {
+		if impl.name == "Redis" {
+			// Skip Redis benchmark for now
+			continue
+		}
+
 		store := impl.create()
 		key := "test_key"
 		value := "test_value"
@@ -14,7 +21,6 @@ func BenchmarkGet(b *testing.B) {
 
 		b.ResetTimer()
 		b.Run(impl.name, func(b *testing.B) {
-
 			for i := 0; i < b.N; i++ {
 				v := store.Get(nil, key)
 				casted, err := v.String()
