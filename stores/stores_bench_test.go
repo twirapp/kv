93d6b0cc4a3acf5d3edbf29fd69688d1718f5c43
Satont <satontworldwide@gmail.com>
2025-10-12 15:18:04 +0300

    chore: add drangonfly to benches

--- stores/stores_bench_test.go
+++ stores/stores_bench_test.go
@@ -2,11 +2,61 @@ package stores
 
 import (
 	"context"
+	"fmt"
+	"os"
 	"testing"
+
+	"github.com/redis/go-redis/v9"
+	tc "github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/wait"
+	"github.com/twirapp/kv"
+	kvredis "github.com/twirapp/kv/stores/redis"
 )
 
+var additionalBenchImplementations = []struct {
+	name   string
+	create func() kv.KV
+}{
+	{
+		name: "Dragonfly (redis client)",
+		create: func() kv.KV {
+			ctx := context.Background()
+			rc, err := tc.Run(
+				ctx,
+				"docker.dragonflydb.io/dragonflydb/dragonfly:latest",
+				tc.WithExposedPorts("6379"),
+				tc.WithWaitStrategy(
+					wait.ForListeningPort("6379"),
+				),
+			)
+			if err != nil {
+				fmt.Printf("Could not start dragonfly container: %v\n", err)
+				os.Exit(1)
+			}
+			connString, err := rc.Endpoint(ctx, "redis")
+			if err != nil {
+				fmt.Printf("Could not get dragonfly connection string: %v\n", err)
+				os.Exit(1)
+			}
+			rOpts, err := redis.ParseURL(connString)
+			if err != nil {
+				fmt.Printf("Could not parse dragonfly connection string: %v\n", err)
+				os.Exit(1)
+			}
+
+			containersLock.Lock()
+			containers = append(containers, rc)
+			containersLock.Unlock()
+
+			return kvredis.New(redis.NewClient(rOpts))
+		},
+	},
+}
+
 func BenchmarkGet(b *testing.B) {
-	for _, impl := range implementations {
+	impls := append(implementations, additionalBenchImplementations...)
+
+	for _, impl := range impls {
 		store := impl.create()
 		key := "test_key"
 		value := "test_value"
